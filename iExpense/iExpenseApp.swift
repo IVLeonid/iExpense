c879a45c85919e9da6dcb891512251c9046b7290
Lenya <leonidivanov826@gmail.com>
2026-04-24 21:44:35 +0300

    Initial Commit

---
+++ iExpense/iExpenseApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseApp.swift
+//  iExpense
+//
+//  Created by Леонід Іванов on 24.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
