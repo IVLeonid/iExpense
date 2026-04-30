b780837a61a56f1bd3ba54aeded69f24dce45494
Lenya <leonidivanov826@gmail.com>
2026-04-30 20:37:49 +0300

    Added data saving via JSONEncoder, also data loading when launching the application, changed the display of information about expenses

--- iExpense/AddView.swift
+++ iExpense/AddView.swift
@@ -8,6 +8,8 @@
 import SwiftUI
 
 struct AddView: View {
+    @Environment(\.dismiss) var dismiss
+    
     @State private var name = ""
     @State private var type = "Personal"
     @State private var amount = 0.0
@@ -15,7 +17,7 @@ struct AddView: View {
     
     let types = ["Business", "Personal"]
     
-    var body: some View {с
+    var body: some View {
         NavigationStack {
             Form {
                 TextField("Name", text: $name)
@@ -30,6 +32,13 @@ struct AddView: View {
                     .keyboardType(.decimalPad)
             }
             .navigationTitle("Add new expense")
+            .toolbar {
+                Button("Save") {
+                    let item = ExpenseItem(name: name, type: type, amount: amount)
+                    expenses.items.append(item)
+                    dismiss()
+                }
+            }
         }
     }
 }
